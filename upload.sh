14fc56298bd66d9a4a4f819a1c6686d842a72759
texascake <budalblimbing@gmail.com>
2024-12-02 13:17:17 +0700

    ci: initial build RROS()

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/sagit/RR*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/sagit/ && ls RR*.zip)"
-      rclone copy ~/rom/out/target/product/sagit/RR*.zip sagit:rr -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/sagit/ && ls RR*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/X00TD/RROS-*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/X00TD/ && ls RROS-*.zip)"
+      rclone copy ~/rom/out/target/product/X00TD/RROS-*.zip Gdrive:rr -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/X00TD/ && ls RROS-*.zip) Uploaded Successfully!"
 fi
