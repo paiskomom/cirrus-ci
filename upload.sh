7cf44fa4cc2692472ec2616ee2723b3fbbbe0917
texascake <budalblimbing@gmail.com>
2025-01-19 11:44:31 +0700

    ci: regen [skip ci]

--- upload.sh
+++ upload.sh
@@ -1,5 +1,6 @@
 if [ -f ~/rom/out/target/product/X00T/lineage-*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d caption="<b>📤 Uploading Build</b> <code>$(cd ~/rom/out/target/product/X00T/ && ls lineage-*.zip)<code>"
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="<b>📤 Uploading Build</b> for <b>`$(cd ~/rom/out/target/product/X00T/ && ls lineage-*.zip)`</b>"
       rclone copy ~/rom/out/target/product/X00T/lineage-*.zip Gdrive:lineage -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d caption="<b>✅ Build $(cd ~/rom/out/target/product/X00T/ && ls lineage-*.zip) Uploaded Successfully!</b>"
+      rclone copy ~/rom/out/target/product/X00T/boot.img Gdrive:lineage -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="<b>🆕 Files<b>`$(cd ~/rom/out/target/product/X00T/ && ls lineage-*.zip)`</b> <b>Uploaded Successfully!</b>"
 fi
