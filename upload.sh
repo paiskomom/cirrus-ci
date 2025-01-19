8169aae964dcf69723d79f0500cd76b98777b321
texascake <budalblimbing@gmail.com>
2025-01-20 02:14:58 +0700

    ci: test build

--- upload.sh
+++ upload.sh
@@ -1,5 +1,6 @@
 if [ -f ~/rom/out/target/product/X00T/Nusantara-*.zip ]; then
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/X00T/ && ls Nusantara-*.zip)"
-      rclone copy ~/rom/out/target/product/X00T/Nusantara-*.zip ewe:nad -P
+      rclone copy ~/rom/out/target/product/X00T/Nusantara-*.zip Gdrive:nad -P
+      rclone copy ~/rom/out/target/product/X00T/boot.img Gdrive:nad -P
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/X00T/ && ls Nusantara-*.zip) Uploaded Successfully!"
 fi
