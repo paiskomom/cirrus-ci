04030c11773c43b3de97a94b95ed5dd7be8d02d5
texascake <budalblimbing@gmail.com>
2025-01-26 10:35:00 +0700

    Update upload.sh [skip ci]

--- upload.sh
+++ upload.sh
@@ -1,8 +1,7 @@
 if [ -f ~/rom/out/target/product/X00T/lineage-*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build for `$(cd ~/rom/out/target/product/X00T/ && ls lineage-*.zip)`"
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build for $(cd ~/rom/out/target/product/X00T/ && ls lineage-*.zip)"
       rclone copy ~/rom/out/target/product/X00T/lineage-*.zip Gdrive:lineage -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="🆕 Zip Files `$(cd ~/rom/out/target/product/X00T/ && ls lineage-*.zip)` Uploaded Successfully!"
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="🆕 Zip Files $(cd ~/rom/out/target/product/X00T/ && ls lineage-*.zip) Uploaded Successfully!"
       rclone copy ~/rom/out/target/product/X00T/boot.img Gdrive:lineage -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="🆕 boot Files `$(cd ~/rom/out/target/product/X00T/ && ls boot.img)` Uploaded Successfully!"
-      curl -s text="Jangan lupa sholat! sebelum disholatkan!!!"
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="🆕 boot Files $(cd ~/rom/out/target/product/X00T/ && ls boot.img) Uploaded Successfully!"
 fi
