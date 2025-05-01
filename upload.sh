0697152361400ee7d83593975587301129cbac3d
texascake <tiktodz@soulvibe-team.cloud>
2025-05-01 08:11:36 +0700

    ci: update tree(s)

--- upload.sh
+++ upload.sh
@@ -1,6 +1,7 @@
 if [ -f ~/rom/out/target/product/X00T/lineage-*.zip ]; then
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build for $(cd ~/rom/out/target/product/X00T/ && ls lineage-*.zip)"
       rclone copy ~/rom/out/target/product/X00T/lineage-*.zip Gdrive:lineage -P
+      rclone copy ~/rom/out/target/product/X00T/boot.img Gdrive:lineage -P
       rclone copy ~/rom/out/build_date.txt Gdrive:lineage -P
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="🆕 Zip Files $(cd ~/rom/out/target/product/X00T/ && ls lineage-*.zip) Uploaded Successfully!"
 fi
