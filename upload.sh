9b3d2fb5808a7b7094e7e20af540153cceacfff8
texascake <tiktodz@soulvibe-team.cloud>
2025-05-28 00:39:56 +0700

    ci: ak

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 if [ -f ~/rom/out/target/product/X00T/Nusantara-*.zip ]; then
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading $(cd ~/rom/out/target/product/X00T/ && ls Nusantara-*.zip)"
-      rclone copy ~/rom/out/target/product/X00T/Nusantara-*.zip Gdrive:Nusantara -P
+      rclone copy ~/rom/out/target/product/X00T/Nusantara-*.zip ak:Nusantara -P
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Boeild Uploaded Successfully!"
 fi
