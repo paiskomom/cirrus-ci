e2e4f167ecfd123364865ba2039ae82087b9350d
paiskomom <midooneteam@gmail.com>
2026-02-21 01:47:22 +0700

    testing

--- upload.sh
+++ upload.sh
@@ -1,6 +1,5 @@
-if [ -f ~/rom/out/target/product/X00T/qassa*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/X00T/ && ls qassa*.zip)"
-      rclone copy ~/rom/out/target/product/X00T/qassa*.zip Gdrive:qassa -P
-      ##rclone copy ~/rom/out/target/product/X00T/boot.img Gdrive:qassa -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/X00T/ && ls qassa*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/whyred/RROS*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/whyred/ && ls RROS*.zip)"
+      rclone copy ~/rom/out/target/product/whyred/RROS*.zip knjt:ccache -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/whyred/ && ls RROS*.zip) Uploaded Successfully!"
 fi
