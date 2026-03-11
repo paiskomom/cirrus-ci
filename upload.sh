62db14768510a7908fb1fc74ecab93965aae35fc
paiskomom <midooneteam@gmail.com>
2026-03-11 12:47:13 +0700

    push

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 if [ -f ~/rom/out/target/product/whyred/RROS*.zip ]; then
-      curl -s https://api.telegram.org/bot$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/whyred/ && ls RROS*.zip)"
-      rclone copy ~/rom/out/target/product/whyred/RROS*.zip knjt:komom -P
-      curl -s https://api.telegram.org/bot$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/whyred/ && ls RROS*.zip) Uploaded Successfully!"
+      curl -s https://api.telegram.org/bot$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/surya/ && ls lineage*.zip)"
+      rclone copy ~/rom/out/target/product/surya/lineage*.zip knjt:kemem -P
+      curl -s https://api.telegram.org/bot$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/surya/ && ls lineage*.zip) Uploaded Successfully!"
 fi
